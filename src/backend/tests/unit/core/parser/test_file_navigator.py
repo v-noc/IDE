4d56b3d595a5bd0a2a60e08679b202fbfd81c6c1
yaredtsy <tsyared@gmail.com>
2025-07-23 01:55:29 +0300

    test improved

--- src/backend/tests/unit/core/parser/test_file_navigator.py
+++ src/backend/tests/unit/core/parser/test_file_navigator.py
@@ -7,12 +7,25 @@ from pathlib import Path
 def test_file_navigator_finds_python_files():
     path = Path(__file__).parent
     sample_project_path = path / "advanced_example"
+
     navigator = FileNavigator(sample_project_path.resolve(), "v-noc.toml")
     python_files = list(navigator.find_files(extensions=['.py']))
-    print(python_files)
+   
     assert len(python_files) == 3
     
     file_names = [os.path.basename(p) for p in python_files]
     assert "main.py" in file_names
     assert "utils.py" in file_names
+    assert "models.py" in file_names
+
+    navigator = FileNavigator(sample_project_path.resolve(), "v-noc-2.toml")
+    python_files = list(navigator.find_files(extensions=['.py']))
+
+    assert len(python_files) == 4
+    file_names = [os.path.basename(p) for p in python_files]
+
     assert "main.py" in file_names
+    assert "utils.py" in file_names
+    assert "models.py" in file_names
+    assert "__init__.py" in file_names
+
