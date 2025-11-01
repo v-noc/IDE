7a42650d522176e1704bd67f14494e1ef9b59177
yaredtsy <tsyared@gmail.com>
2025-11-01 05:16:19 +0300

    test fixed

--- src/backend/tests/unit/parser/analyzer/calls/simple_calls/main.py
+++ src/backend/tests/unit/parser/analyzer/calls/simple_calls/main.py
@@ -1,17 +1,17 @@
 def reader(doc):
-    """ID: d2087931-f419-40d8-98b4-a5cdafc24a6b"""
+    """ID: f7a58565-3aa0-4628-9736-699875ec3ff5"""
     doc.read()
 
 class Document:
-    """ID: 52dfc4ca-897b-4f03-9ee8-6ab999d14fc0"""
+    """ID: 6dfd80cd-f485-4caf-a79c-e0e833cc8349"""
     def read(self):
-        """ID: ea792511-d04b-4763-8fee-ec73c4140ec3"""
+        """ID: 8fab7d52-5771-47bc-bde3-fbf866188412"""
         pass
 
 class FileReader:
-    """ID: d3b75cbc-3215-4339-8ef6-cb8979cfb330"""
+    """ID: 42ea4134-8d33-4423-9b51-0950f4de3b4a"""
     def read(self, file_name: str):
-        """ID: 2020758c-0efc-4ccf-902f-5296f40f2021"""
+        """ID: 81e37c5a-b940-46f7-9512-2635016bed3b"""
         pass
 
 a = Document()
