a82aa154b8ca9333bea3d446f0f2189ea6134825
yaredtsy <tsyared@gmail.com>
2026-01-04 13:31:32 +0300

    import test fix

--- src/backend/tests/unit/parser/analyzer/import/test_import.py
+++ src/backend/tests/unit/parser/analyzer/import/test_import.py
@@ -1,10 +1,11 @@
+import pytest
+
 from app.core.schemas.tree import (
-    ProjectNode,
-    FileTreeNode,
     AnyTreeNode,
     CallTreeNode,
+    FileTreeNode,
+    ProjectTreeNode,
 )
-import pytest
 
 
 def find_file_node(nodes: list[AnyTreeNode], file_name: str) -> FileTreeNode | None:
@@ -19,7 +20,7 @@ def find_file_node(nodes: list[AnyTreeNode], file_name: str) -> FileTreeNode | N
 
 
 @pytest.mark.asyncio
-async def test_absolute_path_import(project_tree: ProjectNode):
+async def test_absolute_path_import(project_tree: ProjectTreeNode):
     file_node = find_file_node(project_tree.children, "import_absolute")
 
     assert file_node is not None
@@ -27,17 +28,15 @@ async def test_absolute_path_import(project_tree: ProjectNode):
 
     # Check for helper.create_user() call
     create_user_call = [
-        node for node in file_node.children if node.name == "create_user"][0]
+        node for node in file_node.children if node.name == "helper.create_user"
+    ][0]
     assert isinstance(create_user_call, CallTreeNode)
-    assert create_user_call.name == "create_user"
+    assert create_user_call.name == "helper.create_user"
     assert create_user_call.target is not None
-    assert (
-        create_user_call.target.qname == "sample_import.utils.helper.create_user"
-    )
+    assert create_user_call.target.qname == "sample_import.utils.helper.create_user"
 
     # Check for User() instantiation
-    user_call = [
-        node for node in file_node.children if node.name == "User"][0]
+    user_call = [node for node in file_node.children if node.name == "User"][0]
     assert isinstance(user_call, CallTreeNode)
     assert user_call.name == "User"
     assert user_call.target is not None
