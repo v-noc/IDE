488e553ff47eed3b56c37a8417eace0cc4a52eaa
yaredtsy <tsyared@gmail.com>
2026-02-13 17:50:54 +0300

    repo improvement

--- src/backend/app/core/repository/code_elements/function_repo.py
+++ src/backend/app/core/repository/code_elements/function_repo.py
@@ -1,10 +1,15 @@
-from typing import List, Dict
-from ..base.base_node_repo import BaseNodeRepository
-from app.core.model.nodes import FunctionNode
-from arangoasync.database import AsyncDatabase
+from app.db.async_terminus_client import AsyncClient
 
 
-class FunctionRepo(BaseNodeRepository[FunctionNode]):
-    def __init__(self, db: AsyncDatabase):
-        super().__init__(db, "nodes", FunctionNode)
+class FunctionRepo():
+    def __init__(self, client: AsyncClient):
+        self.client = client
 
+    def get_function_by_id(self, function_id: str):
+        pass
+
+    def get_function_by_filed(self, field_name: str, field_value: str):
+        pass
+
+    def get_children(self, function_id: str, child_type: str):
+        pass
